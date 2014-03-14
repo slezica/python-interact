5d7c2c7f2263d34924ac99c4ee8941aeab5dfad5
Santiago Lezica <slezica89@gmail.com>
2014-03-14 19:32:38 -0300

    Increased version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name     = 'interact',
-    version  = '0.1',
+    version  = '0.2',
     url      = 'https://github.com/slezica/python-interact',
 
     author       = 'Santiago Lezica',
